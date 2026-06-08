24138ea19606fb20049e5f36011238654c6b3674
anacanocustodio-a11y <ana.cano.custodio@escola.pr.gov.br>
2026-06-08 09:54:00 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// Função que exibe um alerta de boas-vindas interativo
+function mostrarMensagem() {
+    alert("Obrigado pelo interesse! O futuro do planeta depende da união entre a produção consciente e a preservação ambiental. 🌿🚜");
+}
